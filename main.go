db784c7fd3f2af3132993b0398df2ab6a146d217
cwon <dream40@nate.com>
2021-05-29 10:52:21 +0900

    bug fix...

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ import (
 	"github.com/gocolly/colly"
 )
 
+//var text_payload = `[{"operationName":"vaccineList","variables":{"input":{"keyword":"코로나백신위탁의료기관","x":"127.1399977","y":"37.264899"},"businessesInput":{"start":0,"display":100,"deviceType":"mobile","x":"127.1399977","y":"37.264899","bounds":"127.1017601;37.2432423;127.1782353;37.2865495","sortingOrder":"distance"},"isNmap":false,"isBounds":false},"query":"query vaccineList($input: RestsInput, $businessesInput: RestsBusinessesInput, $isNmap: Boolean!, $isBounds: Boolean!) {\n  rests(input: $input) {\n    businesses(input: $businessesInput) {\n      total\n      vaccineLastSave\n      isUpdateDelayed\n      items {\n        id\n        name\n        dbType\n        phone\n        virtualPhone\n        hasBooking\n        hasNPay\n        bookingReviewCount\n        description\n        distance\n        commonAddress\n        roadAddress\n        address\n        imageUrl\n        imageCount\n        tags\n        distance\n        promotionTitle\n        category\n        routeUrl\n        businessHours\n        x\n        y\n        imageMarker @include(if: $isNmap) {\n          marker\n          markerSelected\n          __typename\n        }\n        markerLabel @include(if: $isNmap) {\n          text\n          style\n          __typename\n        }\n        isDelivery\n        isTakeOut\n        isPreOrder\n        isTableOrder\n        naverBookingCategory\n        bookingDisplayName\n        bookingBusinessId\n        bookingVisitId\n        bookingPickupId\n        vaccineQuantity {\n          quantity\n          quantityStatus\n          vaccineType\n          vaccineOrganizationCode\n          __typename\n        }\n        __typename\n      }\n      optionsForMap @include(if: $isBounds) {\n        maxZoom\n        minZoom\n        includeMyLocation\n        maxIncludePoiCount\n        center\n        __typename\n      }\n      __typename\n    }\n    queryResult {\n      keyword\n      vaccineFilter\n      categories\n      region\n      isBrandList\n      filterBooking\n      hasNearQuery\n      isPublicMask\n      __typename\n    }\n    __typename\n  }\n}\n"}]`
 var text_payload = `[{"operationName":"vaccineList","variables":{"input":{"keyword":"코로나백신위탁의료기관","x":"Longitude","y":"Latitude"},"businessesInput":{"start":0,"display":100,"deviceType":"mobile","x":"Longitude","y":"Latitude","bounds":"LonB1;LatB1;LonB2;LatB2","sortingOrder":"distance"},"isNmap":false,"isBounds":false},"query":"query vaccineList($input: RestsInput, $businessesInput: RestsBusinessesInput, $isNmap: Boolean!, $isBounds: Boolean!) {\n  rests(input: $input) {\n    businesses(input: $businessesInput) {\n      total\n      vaccineLastSave\n      isUpdateDelayed\n      items {\n        id\n        name\n        dbType\n        phone\n        virtualPhone\n        hasBooking\n        hasNPay\n        bookingReviewCount\n        description\n        distance\n        commonAddress\n        roadAddress\n        address\n        imageUrl\n        imageCount\n        tags\n        distance\n        promotionTitle\n        category\n        routeUrl\n        businessHours\n        x\n        y\n        imageMarker @include(if: $isNmap) {\n          marker\n          markerSelected\n          __typename\n        }\n        markerLabel @include(if: $isNmap) {\n          text\n          style\n          __typename\n        }\n        isDelivery\n        isTakeOut\n        isPreOrder\n        isTableOrder\n        naverBookingCategory\n        bookingDisplayName\n        bookingBusinessId\n        bookingVisitId\n        bookingPickupId\n        vaccineQuantity {\n          quantity\n          quantityStatus\n          vaccineType\n          vaccineOrganizationCode\n          __typename\n        }\n        __typename\n      }\n      optionsForMap @include(if: $isBounds) {\n        maxZoom\n        minZoom\n        includeMyLocation\n        maxIncludePoiCount\n        center\n        __typename\n      }\n      __typename\n    }\n    queryResult {\n      keyword\n      vaccineFilter\n      categories\n      region\n      isBrandList\n      filterBooking\n      hasNearQuery\n      isPublicMask\n      __typename\n    }\n    __typename\n  }\n}\n"}]`
 
 func main() {
@@ -103,12 +104,16 @@ func get_rest_vaccine_data(quit chan bool, interval_time uint, latitude float64,
 				jsonparser.ArrayEach(r.Body, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
 					name, _ := jsonparser.GetString(value, "name")
 					road, _ := jsonparser.GetString(value, "roadAddress")
-					vq, _ := jsonparser.GetInt(value, "vaccineQuantity", "quantity")
-					if vq > 0 {
-						beeep.Notify("vaccineQuantity > 0 !", name+road, "")
+					vq, err := jsonparser.GetString(value, "vaccineQuantity", "quantity")
+					q, err := strconv.Atoi(vq)
+					if err == nil {
+						if q > 0 {
+							beeep.Notify("vaccineQuantity > 0 !", name+road, "")
+						}
 					}
 
 				}, "[0]", "data", "rests", "businesses", "items")
+				fmt.Println("iter")
 			})
 			c.PostRaw("https://api.place.naver.com/graphql", []byte(payload))
 		case <-quit:
